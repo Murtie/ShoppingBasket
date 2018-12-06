203eff273abddbe932e5ac46534fd62ba2d19133
Murtaza <Murtaza@R-03397>
2018-12-06 01:32:48 +0000

    First pass for Scenario 1 Given the basket has 1 bread, 1 butter and 1 milk when I total, the basket then the total should be £2.95. Refactor required for applying SOLID Design Principles.

--- ShoppingBasket.Library/Basket.cs
+++ ShoppingBasket.Library/Basket.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace ShoppingBasket.Library
 {
+    ///
+    /// Not following SOLID Design Principles
+    /// Not open for extension but open for modification - OCP Fail
+    /// Not following Dependency Inversion Principle - tightly coupled with BasketItem Class
     public class Basket
     {
+        private readonly List<BasketItem> _basketitems;
+
+        public Basket()
+        {
+            _basketitems = new List<BasketItem>();
+        }
+
+        public List<BasketItem> BasketItems
+        {
+            get { return _basketitems; }
+        }
+
+
+        public void AddItem(BasketItem basketItem)
+        {
+            _basketitems.Add(basketItem);
+        }
+
+        public decimal Total()
+        {
+            decimal total = 0m;
+            total = _basketitems.Sum(m => m.Price * m.Quantity);
+            return total;
+        }
+
     }
 }
--- ShoppingBasket.Library/BasketItem.cs
+++ ShoppingBasket.Library/BasketItem.cs
@@ -6,7 +6,26 @@ using System.Threading.Tasks;
 
 namespace ShoppingBasket.Library
 {
+    /// <summary>
+    /// Not following SOLID Design Principles
+    /// Not open for extension but open for modification - OCP Fail
+    /// Creates dependency on this Class wherever referenced - DIP Fail
+    /// </summary>
     public class BasketItem
     {
+        public decimal Price
+        {
+            get; set;
+        }
+
+        public string ProductName
+        {
+            get; set;
+        }
+
+        public int Quantity
+        {
+            get; set;
+        }
     }
 }
